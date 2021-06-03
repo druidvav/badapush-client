09a1a20247659990d5f2225e1b04e40d92cb5f2b
Anton Vlasov <druidvav@gmail.com>
2021-06-03 15:24:56 +0300

    batch enqueue

--- src/Druidvav/BadapushClient/BadapushClient.php
+++ src/Druidvav/BadapushClient/BadapushClient.php
@@ -32,12 +32,9 @@ class BadapushClient
      * @throws InvalidPayloadException
      * @throws InvalidSubscribeIdException
      */
-    public function sendPayload(PayloadInterface $payload)
+    public function sendPayload(PayloadInterface $payload): string
     {
-        $ch = curl_init($this->apiUrl);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 15);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
+        $result = $this->request([
             'id' => 1,
             'method' => $this->method,
             'params' => [
@@ -46,22 +43,66 @@ class BadapushClient
                 'is_development' => $payload->isDevelopment(),
                 'external_id' => $payload->getExternalId()
             ]
-        ]));
+        ]);
+        return !empty($result['response']) ? $result['response'] : 'ok';
+    }
+
+    /**
+     * @param int $fromId
+     * @return array|Message[]
+     * @throws ClientException
+     * @throws InternalErrorException
+     */
+    public function retrieveMessages($fromId = 0): array
+    {
+        $result = $this->request([
+            'id' => 1,
+            'method' => 'payload.getMessages',
+            'params' => [ 'from' => $fromId ]
+        ]);
+        $response = [ ];
+        foreach ($result['list'] as $row) {
+            $response[] = new Message($row);
+        }
+        return $response;
+    }
+
+    /**
+     * @param $externalId
+     * @param string $reason
+     * @throws ClientException
+     * @throws InternalErrorException
+     */
+    public function cancelCallsByExternalId($externalId, $reason = '')
+    {
+        $this->request([
+            'id' => 1,
+            'method' => 'caller.cancel',
+            'params' => [ 'external_id' => $externalId, 'reason' => $reason ]
+        ]);
+    }
+
+    protected function request($query)
+    {
+        $ch = curl_init($this->apiUrl);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($query));
         curl_setopt($ch, CURLOPT_HTTPHEADER, [
             'X-Authorization-Token: ' . $this->apiKey,
             'Content-Type: application/json',
         ]);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($ch);
+        $responseData = curl_exec($ch);
         $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         $errno = curl_errno($ch);
         $error = curl_error($ch);
         curl_close($ch);
 
-        $data = json_decode($response, true);
+        $data = json_decode($responseData, true);
         if (!empty($data['result']['result'])) {
             if ($data['result']['result'] == 'ok') {
-                return !empty($data['result']['response']) ? $data['result']['response'] : 'ok';
+                return $data['result'];
             } elseif ($data['result']['result'] == 'error') {
                 switch ($data['result']['error_code']) {
                     default: throw new ClientException($data['result']['error_message']);
@@ -79,102 +120,6 @@ class BadapushClient
         } elseif ($errno == 28) {
             throw new InternalErrorException('TIMEOUT ' . $error);
         }
-        throw new ClientException($httpcode . '/' . $errno . ': ' . ($error ?: $response));
-    }
-
-    /**
-     * @param int $fromId
-     * @return array|Message[]
-     * @throws ClientException
-     * @throws InternalErrorException
-     */
-    public function retrieveMessages($fromId = 0)
-    {
-        $ch = curl_init($this->apiUrl);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 15);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
-            'id' => 1,
-            'method' => 'payload.getMessages',
-            'params' => [ 'from' => $fromId ]
-        ]));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'X-Authorization-Token: ' . $this->apiKey,
-            'Content-Type: application/json',
-        ]);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $responseData = curl_exec($ch);
-        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        $errno = curl_errno($ch);
-        $error = curl_error($ch);
-        curl_close($ch);
-
-        $data = json_decode($responseData, true);
-        if (!empty($data['result']['result'])) {
-            if ($data['result']['result'] == 'ok') {
-                $response = [ ];
-                foreach ($data['result']['list'] as $row) {
-                    $response[] = new Message($row);
-                }
-                return $response;
-            } elseif ($data['result']['result'] == 'error') {
-                throw new ClientException($data['result']['error_message']);
-            }
-        } elseif (!empty($data['error'])) {
-            throw new ClientException($data['error']['code'] . ': ' . $data['error']['message']);
-        } elseif ($httpcode == 502) {
-            throw new InternalErrorException('Service is temporary shut down');
-        } elseif ($httpcode == 504) {
-            throw new InternalErrorException('Service is temporary down');
-        } elseif ($errno == 28) {
-            throw new InternalErrorException('TIMEOUT ' . $error);
-        }
         throw new ClientException($httpcode . '/' . $errno . ': ' . ($error ?: $responseData));
-    }
-
-    /**
-     * @param $externalId
-     * @param string $reason
-     * @throws ClientException
-     * @throws InternalErrorException
-     */
-    public function cancelCallsByExternalId($externalId, $reason = '')
-    {
-        $ch = curl_init($this->apiUrl);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 15);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
-            'id' => 1,
-            'method' => 'caller.cancel',
-            'params' => [ 'external_id' => $externalId, 'reason' => $reason ]
-        ]));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'X-Authorization-Token: ' . $this->apiKey,
-            'Content-Type: application/json',
-        ]);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($ch);
-        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        $errno = curl_errno($ch);
-        $error = curl_error($ch);
-        curl_close($ch);
-
-        $data = json_decode($response, true);
-        if (!empty($data['result']['result'])) {
-            if ($data['result']['result'] == 'ok') {
-                return;
-            } elseif ($data['result']['result'] == 'error') {
-                throw new ClientException($data['result']['error_message']);
-            }
-        } elseif (!empty($data['error'])) {
-            throw new ClientException($data['error']['code'] . ': ' . $data['error']['message']);
-        } elseif ($httpcode == 502) {
-            throw new InternalErrorException('Service is temporary shut down');
-        } elseif ($httpcode == 504) {
-            throw new InternalErrorException('Service is temporary down');
-        } elseif ($errno == 28) {
-            throw new InternalErrorException('TIMEOUT ' . $error);
-        }
-        throw new ClientException($httpcode . '/' . $errno . ': ' . ($error ?: $response));
     }
 }
--- src/Druidvav/BadapushClient/BadapushQueueClient.php
+++ src/Druidvav/BadapushClient/BadapushQueueClient.php
@@ -1,7 +1,47 @@
 <?php
 namespace Druidvav\BadapushClient;
 
+use Druidvav\BadapushClient\Payload\PayloadInterface;
+
 class BadapushQueueClient extends BadapushClient
 {
     protected $method = 'payload.enqueue';
+
+    protected $batchMode = false;
+    protected $batch = [ ];
+
+    public function beginBatch(): void
+    {
+        $this->batchMode = true;
+    }
+
+    public function sendPayload(PayloadInterface $payload): string
+    {
+        if ($this->batchMode) {
+            $this->batch[] = $payload;
+            return 'ok';
+        } else {
+            return parent::sendPayload($payload);
+        }
+    }
+
+    public function commitBatch(): void
+    {
+        $batch = [ ];
+        foreach ($this->batch as $payload) {
+            $batch[] = [
+                'device_id' => $payload->getDeviceId(),
+                'payload' => $payload->getPayload(),
+                'is_development' => $payload->isDevelopment(),
+                'external_id' => $payload->getExternalId()
+            ];
+        }
+        $result = $this->request([
+            'id' => 1,
+            'method' => 'payload.enqueueBatch',
+            'params' => [ $batch ]
+        ]);
+        $this->batchMode = false;
+        $this->batch = [ ];
+    }
 }
